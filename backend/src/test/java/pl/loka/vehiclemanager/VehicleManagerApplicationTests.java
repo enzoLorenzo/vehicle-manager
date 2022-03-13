f1f830a7ba30de78f700a860ee69c7608d55f1ec
enzoLorenzo <szymonxlorenc@gmail.com>
2022-03-13 16:37:54 +0100

    initial commit

---
+++ backend/src/test/java/pl/loka/vehiclemanager/VehicleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.loka.vehiclemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
