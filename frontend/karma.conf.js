af48b3c22549192caaf80129a11d0bccd18c3130
enzoLorenzo <szymonxlorenc@gmail.com>
2022-04-10 16:15:13 +0200

    fixed ci

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/frontend'),
+      dir: require('path').join(__dirname, './coverage/vehicle-manager-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
