8309449084ff29ad3496f95b1e93c3dfeef197c4
mkapust8 <marcin.kapusta@gazeta.pl>
2022-04-24 19:05:01 +0200

    Added Vehicle card

---
+++ frontend/src/app/modules/main/pages/vehicles/vehicle-card/vehicle-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-card',
+  templateUrl: './vehicle-card.component.html',
+  styleUrls: ['./vehicle-card.component.scss']
+})
+export class VehicleCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
