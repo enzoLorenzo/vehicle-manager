0c3bdaba383acbdf690773f0c22f97af175ec324
mkapust8 <marcin.kapusta@gazeta.pl>
2022-05-15 15:19:27 +0200

    Cleaned code ofdialog-add-repair-component.ts

--- frontend/src/app/modules/main/pages/repairs/dialog-add-repair/dialog-add-repair.component.ts
+++ frontend/src/app/modules/main/pages/repairs/dialog-add-repair/dialog-add-repair.component.ts
@@ -1,15 +1,11 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RepairPost, TaskStatus} from "../../../models/task";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
-
 import {FileService} from "../../../services/file.service";
-
 import {switchMap} from "rxjs/operators";
 import {Observable, tap} from "rxjs";
-import {Vehicle} from "../../../models/vehicle";
 import {Workshop} from "../../../models/workshop";
-import {VehicleApiService} from "../../../services/vehicle-api.service";
 import {WorkshopApiService} from "../../../services/workshop-api.service";
 import {TaskApiService} from "../../../services/task-api.service";
 
@@ -22,37 +18,7 @@ export class DialogAddRepairComponent implements OnInit {
 
   keys = Object.keys;
   startDate = new Date(2022, 5, 14);
-  vehicles: Vehicle[] = [];
   workshops: Workshop[] = [];
-  vehicle: Vehicle = {} as Vehicle;
-  workshop: Workshop = {} as Workshop;
-
-
-
-  private getVehicles() {
-    this.vehicleApiService.getClientVehicle()
-      .subscribe((vehicles: Vehicle[]) => {
-        this.vehicles = vehicles;
-        //console.log('inside' + this.vehicles.length);
-      });
-  }
-
-  private getVehicle(id: number){
-    this.vehicleApiService.getClientSingleVehicle(id)
-      .subscribe((vehicle: Vehicle) => {
-        //this.vehicles.push(vehicle);
-        this.vehicle = vehicle;
-        console.log('inside' + this.vehicle.registration);
-      });
-  }
-
-  private getWorkshop(id: number){
-    this.workshopApiService.getClientSingleWorkshop(id)
-      .subscribe((workshop: Workshop) => {
-        this.workshop = workshop;
-        //console.log('inside' + this.workshop.name);
-      });
-  }
 
   private getWorkshops() {
     this.workshopApiService.getClientWorkshop()
@@ -69,15 +35,9 @@ export class DialogAddRepairComponent implements OnInit {
     vehicleIdFC: new FormControl('', Validators.required)
   });
 
-  selectedFiles?: FileList;
-
-  @ViewChild("inputImage")
-  inputImage!: ElementRef
-
   constructor(
     public dialogRef: MatDialogRef<DialogAddRepairComponent>,
     private fileService: FileService,
-    private vehicleApiService: VehicleApiService,
     private workshopApiService: WorkshopApiService,
     private repairApiService: TaskApiService
   ) {
@@ -85,37 +45,15 @@ export class DialogAddRepairComponent implements OnInit {
   }
 
   submit(): void {
-    this.getVehicle(this.repairFG.get("vehicleIdFC")!.value)
-    //this.getWorkshop(1);
-    //const data = Date(this.repairFG.get("startDateFG")!.value)
-    //const data = Date.parse(this.repairFG.get("startDateFG")!.value)
-    //const data = new Date('2005-02-01')
-    const newRepair: RepairPost = {
 
+    const newRepair: RepairPost = {
       description: this.repairFG.get("descriptionFC")!.value,
       startDate: this.repairFG.get("startDateFC")!.value,
       endDate: this.repairFG.get("endDateFC")!.value,
       taskStatus: TaskStatus.PENDING,
       vehicleId: this.repairFG.get("vehicleIdFC")!.value,
       workshopId: this.repairFG.get("workshopFC")!.value.id
-
     }
-    //this.getVehicles();
-    //this.getVehicle(1);
-
-
-    // console.log(this.repairFG.get('startDateFC')!.value);
-    // console.log(this.repairFG.get('endDateFC')!.value);
-    // // console.log(this.startDate);
-    // // console.log(data);
-    // console.log('outside' + this.vehicles.length);
-    // console.log(this.vehicle.registration)
-    // console.log(this.workshop.name)
-
-
-
-
-
 
     this.repairApiService.addClientRepair(newRepair).pipe(
       switchMap(value => {
@@ -130,21 +68,7 @@ export class DialogAddRepairComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //this.getVehicles();
     this.getWorkshops();
-  }
-
-  selectFile(event: any) {
-    this.selectedFiles = event.target.files;
-  }
-
-  cleanInput() {
-    this.inputImage.nativeElement.value = "";
-    this.selectedFiles = undefined;
-  }
-
-  private uploadImage(id: number) {
-    return this.fileService.onUpload(id, this.selectedFiles!.item(0)!)
   }
 
 }
